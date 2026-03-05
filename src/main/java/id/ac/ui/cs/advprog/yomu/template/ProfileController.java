3f634908ade8d17ae766b21231422dc349081962
hasanul muttaqin <hasanul.muttaqin@ui.ac.id>
2026-03-05 14:52:35 +0700

    chore(profile): scaffold profile endpoint skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/yomu/template/ProfileController.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.yomu.template;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProfileController {
+
+    @GetMapping("/profile")
+    public String profile() {
+        return "profile/index";
+    }
+}
